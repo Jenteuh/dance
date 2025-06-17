3291cec541b6efb5a10a17f0bc704c0f50e6b741
Jente Huysmans <jentehuysmans@hotmail.com>
2025-06-17 15:03:18 +0200

    DataSource

--- src/main/java/be/vdab/dance/DanceApplication.java
+++ src/main/java/be/vdab/dance/DanceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DanceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DanceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DanceApplication.class, args);
+    }
 
 }
